8a85428e6680903f7cfdc19976530eba4ccd0258
dineshmurani <51493204+dineshmurani@users.noreply.github.com>
2025-10-20 11:27:19 -0700

    208 Definitions as API response

--- api.py
+++ api.py
@@ -1,5 +1,8 @@
 import justpy as jp
 
+import definition
+
+
 class Api:
     """Handles requests at /api?w=word
     """
@@ -7,7 +10,10 @@ class Api:
     def serve(cls, req):
         wp = jp.WebPage()
         word = req.query_params.get('w')
-        jp.Div(a=wp, text=word.title())
+
+        defined = definition.Definition(word).get()
+
+        wp.html = defined
         return wp
 
 jp.Route("/api", Api.serve)
